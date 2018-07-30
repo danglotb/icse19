1809250f994cb5da2da58189d7ac40065786c841
danglotb <bdanglot@gmail.com>
2018-07-30 16:40:47 +0200

    script ready for selection

--- src/main/python/august-2018/select-tests.py
+++ src/main/python/august-2018/select-tests.py
@@ -2,18 +2,21 @@ import sys
 import toolbox
 import os.path
 
-def run(project):
+
+def run(project, lower_bound=1, upper_bound=-1):
     result = toolbox.get_all_branches_of_bugs(project)
-    for res in result[23:24]:
+    print lower_bound, upper_bound
+    for res in result[lower_bound:upper_bound]:
         run_one(project, res)
 
 def run_one(project, branch):
-    path_to_csv = project + "_" + branch.split("/")[-1] + ".csv"
+    # getting the concerned module
+    targetModule = toolbox.get_module(project)
+    toolbox.current_output_log = os.path.abspath(toolbox.prefix_dataset + project + "/log_test-selection_" + branch.split("/")[-1] + ".log")
+    path_to_csv ="../../" + ("../" if not targetModule == "" else "") + project +  "/data_test-selection_" + branch.split("/")[-1] + ".csv"
     if os.path.isfile(path_to_csv):
         return
     toolbox.initialize_project_for_branch(project, branch)
-    # getting the concerned module
-    targetModule = toolbox.get_module(project)
     # run maven plugin to compute the list
     code = toolbox.print_and_call(
         " ".join(
@@ -23,13 +26,15 @@ def run_one(project, branch):
              "-Dincludes=junit:junit",
              "eu.stamp-project:diff-test-selection:0.2:list",
              "-DpathToDiff=" + ("../" if not targetModule == "" else "") + toolbox.relative_patch_path,
-             "-DpathToOtherVersion=../" + ("../" if not targetModule == "" else "") + project + toolbox.suffix_project + "/",
-             "-DoutputPath=../../" + ("../" if not targetModule == "" else "") + path_to_csv,
+             "-DpathToOtherVersion=../" + (
+             "../" if not targetModule == "" else "") + project + toolbox.suffix_project + "/",
+             "-DoutputPath=" + path_to_csv,
              "-Dreport=CSV"]
         )
     )
     print code
     toolbox.print_and_call(" ".join(["rm", "-rf", project + toolbox.suffix_project]))
+
 
 if __name__ == '__main__':
 
@@ -38,4 +43,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print "usage python select-tests.py <project>"
     else:
-        run(sys.argv[1])
+        if len(sys.argv) > 2:
+            run(sys.argv[1], lower_bound=int(sys.argv[2]), upper_bound=int(sys.argv[3]))
+        else:
+            run(sys.argv[1])
--- src/main/python/august-2018/toolbox.py
+++ src/main/python/august-2018/toolbox.py
@@ -1,5 +1,9 @@
 import subprocess
 import csv
+import shlex
+import logging
+import subprocess
+from StringIO import StringIO
 
 absolute_path_dspot = "/home/bdanglot/workspace/dspot/dspot/target/dspot-1.1.1-SNAPSHOT-jar-with-dependencies.jar"
 prefix_dataset = "dataset/august-2018/"
@@ -9,6 +13,7 @@ prefix_result = "results/august-2018/"
 suffix_project = "_fixed"
 relative_patch_path = ".bugs-dot-jar/developer-patch.diff"
 maven_home = ""
+current_output_log = ""
 
 
 def readTestToBeExecuted(project, branch):
@@ -40,13 +45,15 @@ def get_module(project):
 
 
 def print_and_call(cmd):
-    print cmd
-    return subprocess.call(cmd, shell=True)
+    global current_output_log
+    with open(current_output_log, "a") as f:
+        return subprocess.call(cmd, shell=True, stdout=f, stderr=f)
 
 
 # this method clean, checkout the correct branch and install.
 # then it copies the project into ../project_fixed and apply the developer patch
 def initialize_project_for_branch(project, branch):
+    output = prefix_dataset + project + "/" + "test-selection_" + branch.split("/")[-1] + ".log"
     global maven_home
     # setting the branch
     print_and_call(" ".join(["cd", prefix_bug_dot_jar + project, "&&",
